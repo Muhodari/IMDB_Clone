c717817a23698ddc5d2bcd0e4df2259bb3bd677e
unknown <sagemuho@gmail.com>
2021-05-12 22:13:07 +0200

    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\StackOverFlow\Desktop\Django\IMDB_Clone\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
