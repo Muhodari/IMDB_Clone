74fa26e4d2cc1ab8937c0308cfd5e20b2d892b4b
unknown <sagemuho@gmail.com>
2021-05-21 19:36:58 +0200

    final project seeting environment

--- IMDB_CLONE/manage.py
+++ IMDB_CLONE/manage.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IMDB_Clone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IMDB_CLONE.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
