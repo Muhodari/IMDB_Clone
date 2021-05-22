d25e05222e478553ba623efc1298f2dd77498523
unknown <sagemuho@gmail.com>
2021-05-22 07:18:22 +0200

    added static and media files

--- IMDB_CLONE/IMDB_CLONE/settings.py
+++ IMDB_CLONE/IMDB_CLONE/settings.py
@@ -118,4 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
