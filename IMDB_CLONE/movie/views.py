6564d9c2fd3d79c8cc81730eeb65e05b28427deb
unknown <sagemuho@gmail.com>
2021-05-26 21:41:21 +0200

    views count,watch,download links

--- IMDB_CLONE/movie/views.py
+++ IMDB_CLONE/movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Movie_Links
 
 
 class MovieList(ListView):
@@ -13,4 +13,13 @@ class MovieList(ListView):
 class MovieDetail(DetailView):
     model = Movie
 
+    def get_object(self):
+        object = super(MovieDetail, self).get_object()
+        object.views_count += 1
+        object.save()
+        return object
 
+    def get_context_data(self, **kwargs):
+        context = super(MovieDetail, self).get_context_data(**kwargs)
+        context['links'] = Movie_Links.objects.filter(movie=self.get_object())
+        return context
