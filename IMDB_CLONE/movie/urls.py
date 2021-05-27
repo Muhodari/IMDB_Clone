47e613f494c7579869d54b633a300359939dc191
unknown <sagemuho@gmail.com>
2021-05-27 22:09:14 +0200

    added movie category

--- IMDB_CLONE/movie/urls.py
+++ IMDB_CLONE/movie/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MovieList, MovieDetail
+from .views import MovieList, MovieDetail, MovieCategory
 
 urlpatterns = [
     path('', MovieList.as_view(), name='movie_list'),
+    path('category/<int:pk>', MovieCategory.as_view(), name='movie_category'),
     path('<int:pk>', MovieDetail.as_view(), name='movie_detail'),
 ]
